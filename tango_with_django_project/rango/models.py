be8b0c19c0df918642445236380a55d1eee45a11
skDn <2038011y@student.gla.ac.uk>
2016-01-30 19:04:38 +0200

    Adding authentication functionality

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+
+    # The additional attributes we wish to include.
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    # Override the __unicode__() method to return out something meaningful!
+    def __unicode__(self):
+        return self.user.username
+
 class Category(models.Model):
         name = models.CharField(max_length=128, unique=True)
         views = models.IntegerField(default=0)
