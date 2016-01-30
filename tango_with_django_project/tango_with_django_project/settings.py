afc1c90d7573a257a0621bd3530bf8a03389d287
skDn <2038011y@student.gla.ac.uk>
2016-01-30 16:20:15 +0200

    Creating a template and integrating it. Setting up static media functionality so that media files can be loaded.

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -26,6 +26,32 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Templates
+
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+)
+
+# Static images
+
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+
+STATIC_URL = '/static/' # You may find this is already defined as such.
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+# Media
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
+
 
 # Application definition
 
